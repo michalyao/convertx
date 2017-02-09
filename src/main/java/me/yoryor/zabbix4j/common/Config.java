6ac81259dd4f609922b91916620b1c98311998ec
michalyao <yaoyao0777@gmail.com>
2017-02-09 14:51:07 +0800

    create: 应用配置类

---
+++ src/main/java/me/yoryor/zabbix4j/common/Config.java
@@ -0,0 +1,24 @@
+package me.yoryor.zabbix4j.common;
+
+import lombok.Data;
+
+@Data
+public class Config {
+    private String packagePath;
+    private String rpcAddress;
+    private HttpConfig httpConfig = new HttpConfig();
+
+    private static Config instance = new Config();
+
+    public static Config getInstance() {
+        return instance;
+    }
+
+    @Data
+    public static class HttpConfig {
+        private int connectTimeout = 5000;
+        private int readTimeout = 30000;
+        private int maxRequestRetry = 2;
+        private int requestTimeout = 5000;
+    }
+}
